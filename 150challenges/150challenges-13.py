4bb44f1d156aa103a6287f4f191fa653c46cfb85
Bradley-Hill <97234446+Bradley-Hill@users.noreply.github.com>
2022-12-23 12:54:21 +0100

    Formatting

--- 150challenges/150challenges-13.py
+++ 150challenges/150challenges-13.py
@@ -1,4 +1,5 @@
 import random
+
 # Challenge 118
 # def UserNumber():
 #     number = int(input('Please enter a number higher than 0: '))
@@ -20,7 +21,7 @@ import random
 # def main():
 #     num = askValue()
 #     counting(num)
-    
+
 # main()
 # Challenge 119
 # def lowTooHigh():
@@ -54,42 +55,48 @@ import random
 # main()
 # Challenge 120
 def addition():
-    number1 = random.randint(5,20)
-    number2 = random.randint(5,20)
-    print(str(number1) + '+' + str(number2) + '= ??')
-    userAnswer = int((input('Please enter the sum here: ')))
+    number1 = random.randint(5, 20)
+    number2 = random.randint(5, 20)
+    print(str(number1) + "+" + str(number2) + "= ??")
+    userAnswer = int((input("Please enter the sum here: ")))
     correctAnswer = number1 + number2
-    answers = userAnswer,correctAnswer
-    return answers
-    
-def subtraction():
-    num1 = random.randint(25,50)
-    num2 = random.randint(1,25)
-    print(str(num1) + '-' + str(num2) + '= ??')
-    userAnswer = int((input('Please enter the solution here: ')))
-    correctAnswer = num1 + num2
-    answers =  userAnswer,correctAnswer
+    answers = userAnswer, correctAnswer
     return answers
 
-def solutionVerify(userAnswer,correctAnswer):
+
+def subtraction():
+    num1 = random.randint(25, 50)
+    num2 = random.randint(1, 25)
+    print(str(num1) + "-" + str(num2) + "= ??")
+    userAnswer = int((input("Please enter the solution here: ")))
+    correctAnswer = num1 + num2
+    answers = userAnswer, correctAnswer
+    return answers
+
+
+def solutionVerify(userAnswer, correctAnswer):
     if userAnswer == correctAnswer:
-        print('Correct! Well done.')
+        print("Correct! Well done.")
     else:
-        print('Incorrect, the correct answer is: ' + str(correctAnswer))
+        print("Incorrect, the correct answer is: " + str(correctAnswer))
+
 
 def main():
-    print(''' 
+    print(
+        """ 
       1) Addition
       2) Subtraction
-      ''')
-    optionChoice = int(input('Please select 1 or 2: '))
+      """
+    )
+    optionChoice = int(input("Please select 1 or 2: "))
     if optionChoice == 1:
         userAnswer, correctAnswer = addition()
-        solutionVerify(userAnswer,correctAnswer)
+        solutionVerify(userAnswer, correctAnswer)
     elif optionChoice == 2:
         userAnswer, correctAnswer = subtraction()
         solutionVerify(userAnswer, correctAnswer)
     else:
-        print('Incorrection selection')
-        
+        print("Incorrection selection")
+
+
 main()
