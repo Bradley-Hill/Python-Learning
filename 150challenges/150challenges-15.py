176dc7ba4abc65b24e5ea50cd468d5834bc5bb07
Bradley-Hill <97234446+Bradley-Hill@users.noreply.github.com>
2023-01-05 12:01:47 +0100

    Got there in the end

--- 150challenges/150challenges-15.py
+++ 150challenges/150challenges-15.py
@@ -6,6 +6,10 @@ from PIL import ImageTk, Image
 
 class MyGUI:
     def __init__(self) -> None:
+        def hello_name():
+            name = text_box.get("1.0", "end")
+            output = str("Hello " + name)
+            output_box["text"] = output
 
         self.root = tk.Tk()
 
@@ -13,9 +17,25 @@ class MyGUI:
         self.root.iconbitmap("icon.ico")
         self.root.geometry("800x600")
 
-        my_image = ImageTk.PhotoImage(Image.open("logo.gif"))
-        my_label = tk.Label(self.root, image=my_image, justify="center")
-        my_label.pack(padx=10, pady=10)
+        my_image = ImageTk.PhotoImage(Image.open("kabuki.jpg"))
+        my_image_label = tk.Label(self.root, image=my_image, justify="center")
+        my_image_label.grid(row=0, column=1)
+
+        label1 = tk.Label(self.root, text="Enter your name: ", font=("Arial", 16))
+        label1.grid(row=1, column=0)
+
+        text_box = tk.Text(self.root, height=1)
+        text_box.grid(row=1, column=1)
+
+        button = tk.Button(
+            self.root, text="Press me", font=("Helvetica", 14), command=hello_name
+        )
+        button.grid(row=2, column=0)
+
+        output_box = tk.Message(
+            self.root,
+        )
+        output_box.grid(row=2, column=1)
 
         self.root.mainloop()
 
