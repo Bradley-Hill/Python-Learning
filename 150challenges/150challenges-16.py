c4b818bc175332498fc3cf4a33c8bc8613654dfd
Bradley-Hill <97234446+Bradley-Hill@users.noreply.github.com>
2023-01-07 14:08:41 +0100

    SQLite3 begins...

--- 150challenges/150challenges-16.py
+++ 150challenges/150challenges-16.py
@@ -1,1 +1,24 @@
+import sqlite3
+
 # Challenge 139
+conn = sqlite3.connect("PhoneBook.db")
+
+# Create a cursor
+c = conn.cursor()
+# Create a Table
+c.execute(
+    """CREATE TABLE phonebook (
+    id integer,
+    first_name text,
+    second_name text,
+    phone_number integer
+)"""
+)
+# Same code but harder to read all on one line.
+# c.execute("CREATE TABLE phonebook (id INTEGER,first_name TEXT,second_name TEXT,phone_number INTEGER)")
+
+# Commit our command
+conn.commit()
+
+# Close the connection
+conn.close()
