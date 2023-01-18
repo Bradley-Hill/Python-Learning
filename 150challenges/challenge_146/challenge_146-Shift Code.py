37961cc00f79e402177b69557c2ea9ce84dc9d67
Bradley-Hill <97234446+Bradley-Hill@users.noreply.github.com>
2023-01-18 16:09:33 +0100

    Tidyied up comments and Docstring

--- 150challenges/challenge_146/challenge_146-Shift Code.py
+++ 150challenges/challenge_146/challenge_146-Shift Code.py
@@ -1,17 +1,5 @@
 from collections import deque
 
-# list1 = ["a", "b", "c", "d", "e", "f", "g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"," "]
-# list1 = deque(list1)
-# list1.rotate(4)
-# list1 = list(list1)
-# print(list1)
-
-# list2 = [1, 2, 3, 4, 5, 6, 7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,]
-# list2 = deque(list2)
-# list2.rotate(4)
-# list2 = list(list2)
-# print(list2)
-
 
 def main():
     """Displays Menu choices"""
